1d5f3ab6d24b2330ac31631f1377d29365aa5933
Arpit <arpitagrawal2402@gmail.com>
2023-11-18 17:54:10 +0530

    Refactor WritePage component

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -13,10 +13,12 @@ import {
   getDownloadURL,
 } from "firebase/storage";
 import { app } from "@/utils/firebase";
-import ReactQuill from "react-quill";
 
 const WritePage = () => {
   const { status } = useSession();
+
+  const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
+
   const router = useRouter();
 
   const [open, setOpen] = useState(false);
@@ -98,64 +100,64 @@ const WritePage = () => {
 
   return (
     <div className={styles.container}>
-            <input
-                type="text"
-                placeholder="Title"
-                className={styles.input}
-                onChange={(e) => setTitle(e.target.value)}
-            />
-            <div className="display-flex">
-                <span className={styles.category} >Category</span>
-                <select className={styles.select} onChange={(e) => setCatSlug(e.target.value)}>
-                    <option value="style">style</option>
-                    <option value="fashion">fashion</option>
-                    <option value="food">food</option>
-                    <option value="culture">culture</option>
-                    <option value="travel">travel</option>
-                    <option value="coding">coding</option>
-                </select>
-            </div>
-            <div className={styles.editor}>
+      <input
+        type="text"
+        placeholder="Title"
+        className={styles.input}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <div className="display-flex">
+        <span className={styles.category} >Category</span>
+        <select className={styles.select} onChange={(e) => setCatSlug(e.target.value)}>
+          <option value="style">style</option>
+          <option value="fashion">fashion</option>
+          <option value="food">food</option>
+          <option value="culture">culture</option>
+          <option value="travel">travel</option>
+          <option value="coding">coding</option>
+        </select>
+      </div>
+      <div className={styles.editor}>
 
-                <div className={styles.addup}>
-                    <button className={styles.button} onClick={() => setOpen(!open)}>
-                        <Image src="/plus.png" alt="" width={16} height={16} />
-                    </button>
-                    {open && (
-                        <div className={styles.add}>
-                            <input
-                                type="file"
-                                id="image"
-                                onChange={(e) => setFile(e.target.files[0])}
-                                style={{ display: "none" }}
-                            />
-                            <button className={styles.addButton}>
-                                <label htmlFor="image">
-                                    <Image src="/image.png" alt="" width={16} height={16} />
-                                </label>
-                            </button>
-                            <button className={styles.addButton}>
-                                <Image src="/external.png" alt="" width={16} height={16} />
-                            </button>
-                            <button className={styles.addButton}>
-                                <Image src="/video.png" alt="" width={16} height={16} />
-                            </button>
-                        </div>
-                    )}
-                </div>
-
-                <ReactQuill
-                    className={styles.textArea}
-                    theme="bubble"
-                    value={value}
-                    onChange={setValue}
-                    placeholder="Tell your story..."
-                />
+        <div className={styles.addup}>
+          <button className={styles.button} onClick={() => setOpen(!open)}>
+            <Image src="/plus.png" alt="" width={16} height={16} />
+          </button>
+          {open && (
+            <div className={styles.add}>
+              <input
+                type="file"
+                id="image"
+                onChange={(e) => setFile(e.target.files[0])}
+                style={{ display: "none" }}
+              />
+              <button className={styles.addButton}>
+                <label htmlFor="image">
+                  <Image src="/image.png" alt="" width={16} height={16} />
+                </label>
+              </button>
+              <button className={styles.addButton}>
+                <Image src="/external.png" alt="" width={16} height={16} />
+              </button>
+              <button className={styles.addButton}>
+                <Image src="/video.png" alt="" width={16} height={16} />
+              </button>
             </div>
-            <button className={styles.publish} onClick={handleSubmit}>
-                Publish
-            </button>
+          )}
         </div>
+
+        <ReactQuill
+          className={styles.textArea}
+          theme="bubble"
+          value={value}
+          onChange={setValue}
+          placeholder="Tell your story..."
+        />
+      </div>
+      <button className={styles.publish} onClick={handleSubmit}>
+        Publish
+      </button>
+    </div>
   );
 };
 
